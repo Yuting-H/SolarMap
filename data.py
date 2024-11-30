d2fa8d52dfc339e305fa8f13ceacfebb6703c454
DanielKaminsky05 <daniel.kaminsky23@gmail.com>
2024-11-30 16:12:32 -0500

    made data into a class

--- data.py
+++ data.py
@@ -1,88 +1,131 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from POWERAPI import createCSV
-from EnergyCalculator import WindEnergyCalculator
-from EnergyCalculator import SolarEnergyCalculator
+from EnergyCalculator import EnergyCalculator
 import concurrent.futures
 import threading
-from test import zipCodetoCoordinates
 
 
-location = zipCodetoCoordinates()
+
+
+
+
+
+class Data:
+    latitude = 0
+    longitude = 0
+    radius = 0
+    windEnergyList = []
+    sunEnergyList = []
+    params_list = []
+    lock = threading.Lock()
+    df_list = []
+    energyCalc = EnergyCalculator()
+
+    def __init__(self, latitude, longitude) -> None:
+        self.latitude = latitude
+        self.longitude = longitude
+        self.radius = 0
+        self.windEnergyList = []
+        self.sunEnergyList = []
+        self.params_list = []
+        self.lock = threading.Lock()
+        self.df_list = []
+        self.energyCalc = EnergyCalculator()
+        self.addPoints()
+
+
+
+
+    def newParams(self,latitude,longitude):
+        params = {
+            "parameters": "ALLSKY_SFC_SW_DWN,WS50M",
+            "community": "RE",
+            "longitude": longitude,
+            "latitude": latitude,
+            "start": "2012",
+            "end": "2022",
+            "format": "CSV"
+        }
+        return params
+
 
     
-longitude = float(location[1])
-latitude = float(location[0])
-radius = 0
+
+    def fetch_data(self, params):
+        data = createCSV(params)
+        with self.lock:
+            with open('data.csv', 'a') as f:
+                f.write(data)
+            df = pd.read_csv('data.csv', skiprows=20)
+            latitude = params.get("latitude")
+            longitude = params.get("longitude")
+            self.df_list.append([df,latitude,longitude])
+                
+        return data
 
 
-windEnergyList = []
-sunEnergyList = []
-params_list = []
-
-def newParams(latitude,longitude):
-    params = {
-        "parameters": "ALLSKY_SFC_SW_DWN,WS50M",
-        "community": "RE",
-        "longitude": longitude,
-        "latitude": latitude,
-        "start": "2012",
-        "end": "2022",
-        "format": "CSV"
-    }
-    return params
-
-
-lock = threading.Lock()
-df_list = []
-
-def fetch_data(params):
-    data = createCSV(params)
-    with lock:
-        with open('data.csv', 'a') as f:
-            f.write(data)
-        df = pd.read_csv('data.csv', skiprows=20)
-        latitude = params.get("latitude")
-        longitude = params.get("longitude")
-        df_list.append([df,latitude,longitude])
-            
-    return data
+    def addPoints(self):
+    
+        self.df_list.clear()
+        self.params_list.clear()
+        if (self.radius == 0):
+            self.params_list.append(self.newParams(self.latitude,self.longitude))
+        else: 
+            self.params_list.append(self.newParams(self.latitude - self.radius,self.longitude + self.radius))
+            self.params_list.append(self.newParams(self.latitude + self.radius,self.longitude - self.radius))
+            self.params_list.append(self.newParams(self.latitude + self.radius,self.longitude + self.radius))
+            self.params_list.append(self.newParams(self.latitude - self.radius,self.longitude - self.radius))
+        self.radius += 1
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = []
+            for param in self.params_list:
+                futures.append(executor.submit(self.fetch_data, param))
+        
 
 
-def addPoints(radius, windEnergyList, sunEnergyList):
-    df_list.clear()
-    params_list.clear()
-    if (radius == 0):
-        params_list.append(newParams(latitude,longitude))
-    else: 
-        params_list.append(newParams(latitude - radius,longitude + radius))
-        params_list.append(newParams(latitude + radius,longitude - radius))
-        params_list.append(newParams(latitude + radius,longitude + radius))
-        params_list.append(newParams(latitude - radius,longitude - radius))
-    radius += 1
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = []
-        for param in params_list:
-            futures.append(executor.submit(fetch_data, param))
+        for df, latitude1, longitude1 in self.df_list:
+            ws50m_df = df[df['PARAMETER'] == 'WS50M']  # Filter rows where PARAMETER is WD50M
+            allsky_df = df[df['PARAMETER'] == 'ALLSKY_SFC_SW_DWN']
+            self.windEnergyList.append([self.energyCalc.WindEnergyCalculator(ws50m_df), latitude1, longitude1])
+            self.sunEnergyList.append([self.energyCalc.SolarEnergyCalculator(allsky_df), latitude1, longitude1])
+
+        
+
+    def getWindEnergyList(self):
+        return self.windEnergyList
+    
+    def getSolarEnergyList(self):
+        return self.sunEnergyList
+    
+    def getSortedWindEnergyList(self):
+        windEnergyList = self.windEnergyList.copy()
+        windEnergyList = sorted(windEnergyList, key=lambda x: x[0][12], reverse=True)
+        return windEnergyList
     
 
+    def getSortedSolarEnergyList(self):
+        solarEnergyList = self.sunEnergyList.copy()
+        solarEnergyList = sorted(solarEnergyList, key=lambda x: x[0][12], reverse=True)
+        return solarEnergyList
+    
+    def getOriginalPointWindEnergy(self):
+        return self.windEnergyList[0][0]
+    
 
-    for df, latitude1, longitude1 in df_list:
-        ws50m_df = df[df['PARAMETER'] == 'WS50M']  # Filter rows where PARAMETER is WD50M
-        allsky_df = df[df['PARAMETER'] == 'ALLSKY_SFC_SW_DWN']
-        windEnergyList.append([WindEnergyCalculator(ws50m_df), latitude1, longitude1])
-        sunEnergyList.append([SolarEnergyCalculator(allsky_df), latitude1, longitude1])
+    def getOriginalPointSolarEnergy(self):
+        return self.sunEnergyList[0][0]
 
 
+    def setRadius(self, radius):
+        self.radius = radius
 
-    windEnergyList = sorted(windEnergyList, key=lambda x: x[0][12], reverse=True)
-    sunEnergyList = sorted(windEnergyList, key=lambda x: x[0][12], reverse=True)
-    print(windEnergyList)
-    return radius
 
-radius = addPoints(radius, windEnergyList, sunEnergyList)
-radius = addPoints(radius, windEnergyList, sunEnergyList)
+    def setLongitude(self, longitude):
+        self.longitude = longitude
 
+    def setLatitude(self, latitude):
+        self.longitude = latitude
 
 
 """
