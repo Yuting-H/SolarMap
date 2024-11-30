527d58a5978562dd4979e4a805dd5aa5e3669b81
yhou294 <yuting.yh@gmail.com>
2024-11-30 15:41:59 -0500

    added backend functions

--- App.py
+++ App.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import EnergyCalculator as ec
+import POWERAPI as papi
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
